2d5c26997ae7f0a0305a6adc77999c47e97f9c72
fuchs-sh <sfuchs@mba2025.hbs.edu>
2024-06-30 19:59:04 -0400

    Add overlay functionality to extension

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,1 +1,5 @@
-console.log("Background script loaded");
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url.includes("practicefusion.com")) {
+    chrome.tabs.sendMessage(tab.id, { action: "toggleOverlay" });
+  }
+});
--- ChromeExtension/contentScript.js
+++ ChromeExtension/contentScript.js
@@ -1,1 +1,18 @@
+import { createOverlay, removeOverlay } from "./overlay.js";
+
 console.log("Content script loaded on Practice Fusion");
+
+// Create the overlay when the page loads
+createOverlay();
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "toggleOverlay") {
+    const overlay = document.getElementById("pf-assistant-overlay");
+    if (overlay) {
+      removeOverlay();
+    } else {
+      createOverlay();
+    }
+  }
+});
